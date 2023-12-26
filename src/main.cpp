5b4c717ce8db7f345a8b1cbceca35a9add0b1a8b
ocai0 <caioalvesfernandes@gmail.com>
2023-12-26 14:58:09 -0300

    Created flappy sprite

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "bn_core.h"
+#include "bn_sprite_ptr.h"
+
+#include "bn_sprite_items_flappy.h"
 
 int main()
 {
     bn::core::init();
+    bn::sprite_ptr sprite = bn::sprite_items::flappy.create_sprite(0, 0);
 
     while(true)
     {
