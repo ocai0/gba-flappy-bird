9820815b54bfddbf9312c3061eb68ff72664eada
ocai0 <caioalvesfernandes@gmail.com>
2025-02-24 14:27:53 -0300

    feat: changed type of MainMenuVars attributes

--- include/MainMenuVars.hpp
+++ include/MainMenuVars.hpp
@@ -1,8 +1,11 @@
 #ifndef MAIN_MENU_VARS_H_
 #define MAIN_MENU_VARS_H_
+    #include "Enum/BirdType.Enum.hpp"
+    #include "Enum/BackgroundType.Enum.hpp"
+    
     typedef struct {
-        int selectedFlappy;
-        int selectedBackground;
-        int selectedPipes;
+        Enum::BirdType birdType;
+        Enum::BackgroundType backgroundType;
+        Enum::PipeType pipeType;
     } MainMenuVars;
 #endif
