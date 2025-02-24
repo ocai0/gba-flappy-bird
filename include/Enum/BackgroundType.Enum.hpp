75c66c7c7110d5c8e76f93bcc7358af2de4d6c88
ocai0 <caioalvesfernandes@gmail.com>
2025-02-24 14:15:49 -0300

    feat: created enums

---
+++ include/Enum/BackgroundType.Enum.hpp
@@ -0,0 +1,8 @@
+#ifndef ENUM_BACKGOUND_TYPE_H_
+#define ENUM_BACKGOUND_TYPE_H_
+    namespace Enum {
+        typedef enum { CITY, ANCIENT, ARENA, DESERT } BackgroundType;
+        typedef BackgroundType PipeType;
+        typedef BackgroundType FloorType;
+    }
+#endif
---
+++ include/Enum/BirdType.Enum.hpp
@@ -0,0 +1,6 @@
+#ifndef ENUM_BIRD_TYPE_H_
+#define ENUM_BIRD_TYPE_H_
+    namespace Enum {
+        typedef enum { FLAPPY_BIRD, ROBOT_BIRD } BirdType;
+    }
+#endif
